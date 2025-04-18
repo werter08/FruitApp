a09cdd7e2beb0099ee756d8439601efe6f792b18
werter <sgot7324@gmail.com>
2025-04-18 23:40:08 +0500

    Starting a new project with swiftUI

---
+++ Fructus/Fructus/App/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Begench on 18.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
